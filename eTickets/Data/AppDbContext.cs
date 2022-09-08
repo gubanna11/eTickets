a1747765fed574bde04e6b7ab02a6dcbec512d2f
Huba Hanna <gubanna.11@gmail.com>
2022-09-08 12:02:41 +0300

    Actors View

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -9,9 +9,7 @@ namespace eTickets.Data
 {
     public class AppDbContext:DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
