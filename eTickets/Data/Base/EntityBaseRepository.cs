a2e60ba5ed36580303155a3f58f5765380ad5b9f
Huba Hanna <gubanna.11@gmail.com>
2022-09-27 20:59:53 +0300

    Producers: Details, Create, IProducersService, ProducersService

--- eTickets/Data/Base/EntityBaseRepository.cs
+++ eTickets/Data/Base/EntityBaseRepository.cs
@@ -16,14 +16,18 @@ namespace eTickets.Data.Base
         }
 
         public async Task AddAsync(T entity)
-            => await _context.Set<T>().AddAsync(entity);
-        
-
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+          
         public async Task DeleteAsync(int id)
         {
             var entity = await _context.Set<T>().FirstOrDefaultAsync(n => n.Id == id);
             EntityEntry entityEntry = _context.Entry<T>(entity);
             entityEntry.State = EntityState.Deleted;
+
+            await _context.SaveChangesAsync();
         }
 
         public async Task<IEnumerable<T>> GetAllAsync()
@@ -37,8 +41,10 @@ namespace eTickets.Data.Base
 
         public async Task UpdateAsync(int id, T entity)
         {
-            EntityEntry entityEntry = _context.Entry<T>(entity);
+            EntityEntry entityEntry =  _context.Entry<T>(entity);
             entityEntry.State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
         }
     }
 }
